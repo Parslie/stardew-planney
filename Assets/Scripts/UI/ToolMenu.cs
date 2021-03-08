8c19d237161e7a508688a85d71826a34bb873277
Viktor Holta <viktor.holta@outlook.com>
2021-03-08 20:19:53 +0100

    NEW: add more trees

---
+++ Assets/Scripts/UI/ToolMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolMenu : MonoBehaviour
+{
+    [SerializeField] private float openingDuration = 0.5f;
+    private float openingT;
+
+    private bool isOpen = false;
+
+    public void ToggleOpen()
+    {
+        isOpen = !isOpen;
+    }
+
+    //////////////////
+    // Unity Functions
+
+    private void Update()
+    {
+        openingT += (isOpen) ? Time.deltaTime / openingDuration : -Time.deltaTime / openingDuration;
+        openingT = Mathf.Clamp(openingT, 0, 1);
+
+        transform.localScale = new Vector3(1, Mathf.SmoothStep(0, 1, openingT), 1);
+    }
+}
